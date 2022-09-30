6505e2a469c1e8f9ffd6fec51b3411c4cb497bfb
tezosmiami <tezosmiami@gmail.com>
2022-09-30 16:21:27 -0400

    case again. . .

--- index.mjs
+++ index.mjs
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/sendTez', (req, res) => {
+app.post('/sendtez', (req, res) => {
     
     let data = req.headers['content-type'] == 'application/json' ? req.body 
         : parse.JSON(req.body) 
